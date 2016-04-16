b422d6302ba37b5a2102c44df83fdfcaaf02b32d
Lijy91 <lijy91@foxmail.com>
2016-04-16 15:48:18 +0800

    增加中文语言包

---
+++ resources/lang/zh-CN/daocloud.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    // 镜像构建
+    'image_build_success' => ':name 镜像构建成功',
+    'image_build_failure' => ':name 镜像构建失败',
+    'image_build_error'   => ':name 镜像构建错误',
+    'image_build_started' => ':name 镜像构建开始',
+    // 持续集成
+    'ci_build_success' => ':name 持续集成执行成功',
+    'ci_build_failure' => ':name 持续集成执行失败',
+    'ci_build_error'   => ':name 持续集成执行错误',
+    'ci_build_started' => ':name 持续集成执行开始',
+];
