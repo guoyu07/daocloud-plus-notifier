18cf0628ba81cbec1881a5641888d7eaeece8097
Lijy91 <lijy91@foxmail.com>
2017-01-26 13:54:59 +0800

    完善状态

--- resources/lang/zh-CN/daocloud.php
+++ resources/lang/zh-CN/daocloud.php
@@ -2,10 +2,13 @@
 
 return [
     // 镜像构建
-    'image_build_success' => ':name 镜像构建成功',
-    'image_build_failure' => ':name 镜像构建失败',
-    'image_build_error'   => ':name 镜像构建错误',
-    'image_build_started' => ':name 镜像构建开始',
+    'image_build_pending'   => '项目 :name 等待执行',
+    'image_build_started'   => '项目 :name 正在执行',
+    'image_build_success'   => '项目 :name 执行成功',
+    'image_build_timeout'   => '项目 :name 执行超时',
+    'image_build_error'     => '项目 :name 执行错误',
+    'image_build_cancelled' => '项目 :name 取消执行',
+    'image_build_failure'   => '项目 :name 执行失败',
     // 持续集成
     'ci_build_success' => ':name 持续集成执行成功',
     'ci_build_failure' => ':name 持续集成执行失败',
